18dfe82b795abfd5575fa88f3ba2fa2e9c53d5f0
vaingle <v.haribhau.ingle@accenture.com>
2024-03-07 19:02:31 +0530

    Update versioning.sh

--- versioning.sh
+++ versioning.sh
@@ -57,7 +57,7 @@ if [[ $branch_name == opsrelease/* ]] && [[ $trigger_event == "create" ]]; then
 elif [[ $branch_name == opsrelease/* ]]; then
     echo "elif 1 triggered"
     version=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout | sed 's/^v//')
-    increment_version $version $increment major
+    increment_version $version $increment minor
 
 elif [[ $branch_name == opshotfix/* ]]; then
     echo "elif 2 triggered"
